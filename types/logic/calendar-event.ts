6153194272acf58436435648b42ba596f5956a8c
emresimsek <emresimseka@gmail.com>
2021-05-19 16:11:43 +0300

    design(calendar-item):

--- types/logic/calendar-event.ts
+++ types/logic/calendar-event.ts
@@ -1,38 +1,39 @@
 import { EnumCalendarDayItemPosition } from '../statics/calendar-day-item-position.enum';
 
-interface CalendarEvent<T = undefined>  {
-    id:string|number
+interface CalendarEvent<T = undefined> {
+    id: string | number
     /**
      * timeSpan
      */
-    from:string
+    from: string
     /**
      * timeSpan
      */
-    to:string
-    title:string
+    to: string
+    title: string
     /**
      * valid css color
      */
-    color:string
-    borderColor?:string
-    textColor?:string
-    isBordered?:boolean
-    borderRadius?:string
-    position:EnumCalendarDayItemPosition
+    color: string
+    borderColor?: string
+    textColor?: string
+    isBordered?: boolean
+    isWraparoundBordered?: boolean
+    borderRadius?: string
+    position: EnumCalendarDayItemPosition
     /**
      * Must be bigger then zero
      */
-    zIndex:number
+    zIndex: number
 
-    isClickable?:boolean
-    isDraggable?:boolean
-    isResizable?:boolean
+    isClickable?: boolean
+    isDraggable?: boolean
+    isResizable?: boolean
 
     /**
      * Data for customization
      */
-    detail?:T
+    detail?: T
 }
 
-export type {CalendarEvent}
+export type { CalendarEvent }
